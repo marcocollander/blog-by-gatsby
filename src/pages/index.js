4f2970885d290c3066d6ecf75d59f68fcd182857
Zenon Marek Raubuć <m.raububc@wp.pl>
2024-05-17 23:02:18 +0200

    Add images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,22 @@
 import * as React from "react"
 
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        src='../images/mountain.jpg'
+        alt='Zdjęcie autorstwa Sagui Andrea z Pexels'
+      />
     </Layout>
   )
 }
 
-export default IndexPage
 
 export const Head = () => <title>Home Page</title>
 
+export default IndexPage
 
