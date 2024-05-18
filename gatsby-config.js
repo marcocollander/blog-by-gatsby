cc96fccc5607530a2f6319888c7e99acb929a254
Zenon Marek Raubuć <m.raububc@wp.pl>
2024-05-18 21:55:38 +0200

    Add posts to blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,19 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `blog-by-gatsby`,
+    title: `Białe to białe, a czarne to czarne`,
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
     'gatsby-plugin-image',
-    'gatsby-plugin-sharp'
+    'gatsby-plugin-sharp',
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+    'gatsby-plugin-mdx'
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
 import * as React from "react"
-
 import Layout from "../components/layout";
 import { StaticImage } from "gatsby-plugin-image";
+import Seo from '../components/seo'
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+    <Layout pageTitle="Blog Marka Raubuć">
       <StaticImage
         src='../images/mountain.jpg'
         alt='Zdjęcie autorstwa Sagui Andrea z Pexels'
@@ -16,7 +15,7 @@ const IndexPage = () => {
 }
 
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => <Seo title="Strona główna" />
 
 export default IndexPage
 
